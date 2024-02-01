495004e54b03ec0fe6efea9c2dcf56a914a47103
Jberma23 <Jesse.ben.berman@gmail.com>
2024-01-31 21:45:14 -0700

    fixing zeitwork errors

--- config/application.rb
+++ config/application.rb
@@ -10,11 +10,12 @@ module TakeoutTaxiApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     # config.load_defaults 7.1
-    config.autoload = :classic
+    config.load_defaults 6.0
+    # config.autoload = :classic
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.assets.initialize_on_precompile = false
+    # config.assets.initialize_on_precompile = false
     # config.autoload_lib(ignore: %w(assets tasks))
     # Configuration for the application, engines, and railties goes here.
     #
