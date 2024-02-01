c9cead88d3612c7df658ccdd4cee2fcd69e9fe52
Jberma23 <Jesse.ben.berman@gmail.com>
2024-01-31 21:04:15 -0700

    adinnng precompile config

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module TakeoutTaxiApi
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.assets.initialize_on_precompile = false
-    # config.autoload_lib(ignore: %w(assets tasks))
+    config.autoload_lib(ignore: %w(assets tasks))
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
