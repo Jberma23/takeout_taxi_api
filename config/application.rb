5bc855e0aee7366d7a7c2e12ac0c5d0ba3bfe1ee
Jberma23 <Jesse.ben.berman@gmail.com>
2024-01-31 21:32:32 -0700

    removing other controllers

--- config/application.rb
+++ config/application.rb
@@ -9,13 +9,13 @@ Bundler.require(*Rails.groups)
 module TakeoutTaxiApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.1
+    # config.load_defaults 7.1
     config.autoload = :classic
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.assets.initialize_on_precompile = false
-    config.autoload_lib(ignore: %w(assets tasks))
+    # config.autoload_lib(ignore: %w(assets tasks))
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
